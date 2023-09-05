5a5c7d8fa3f9459d0b6ecc725961d24c0cc35932
Fabrithegreat <fabrixio.rodrigo@gmail.com>
2023-09-05 15:41:02 +0200

    init

---
+++ src/test/java/se/fabricioflores/labthree/MainTest.java
@@ -0,0 +1,11 @@
+package se.fabricioflores.labthree;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    void test() {
+        assertThat(false).isFalse();
+    }
+}
