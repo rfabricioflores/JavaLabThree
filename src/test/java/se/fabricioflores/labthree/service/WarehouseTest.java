12bcc14ba6fcc4186f2b87d23ee3272871614de5
Fabricio Flores <me@fabricioflores.se>
2023-09-16 20:12:19 +0200

    added all remaining tests

--- src/test/java/se/fabricioflores/labthree/service/WarehouseTest.java
+++ src/test/java/se/fabricioflores/labthree/service/WarehouseTest.java
@@ -6,7 +6,9 @@ import se.fabricioflores.labthree.entities.Product;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.assertj.core.api.Assertions.*;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WarehouseTest {
     private Warehouse warehouse;
@@ -16,10 +18,10 @@ public class WarehouseTest {
         warehouse = new Warehouse();
     }
 
-    //    Add Product Tests
+    // Add Product Tests
     @Test
     void testAddProduct() {
-        var product = new Product(1, "Product 1", Category.TECH, 8);
+        var product = new Product(1, "Product 1", Category.TECH, 5);
         warehouse.addProduct(product);
 
         var allProducts = warehouse.getAllProducts();
@@ -30,7 +32,7 @@ public class WarehouseTest {
 
     @Test
     void testAddProductWithEmptyName() {
-        var product = new Product(1, "", Category.SPORT, 2);
+        var product = new Product(1, "", Category.SPORT, 5);
         warehouse.addProduct(product);
 
         var allProducts = warehouse.getAllProducts();
@@ -41,7 +43,7 @@ public class WarehouseTest {
     @Test
     void testAddTwoProductsWithSameId() {
         var product1 = new Product(1, "Ring", Category.JEWELRY, 2);
-        var product2 = new Product(1, "Necklace", Category.SPORT, 5);
+        var product2 = new Product(1, "Ball", Category.SPORT, 5);
 
         warehouse.addProduct(product1);
         warehouse.addProduct(product2);
@@ -50,5 +52,115 @@ public class WarehouseTest {
 
         assertThat(allProducts).hasSize(1);
         assertThat(allProducts).containsExactly(product1);
+    }
+
+
+    // Edit Product Tests
+    @Test
+    void testEditProduct() {
+        var product = new Product(1, "Product One", Category.SPORT, 2);
+        warehouse.addProduct(product);
+
+        warehouse.editProduct(1, "Edited Product", Category.TECH, 3);
+
+        var optionalProduct = warehouse.getProductByID(1);
+        assertThat(optionalProduct).isPresent();
+
+        var updatedProduct = optionalProduct.get();
+        assertThat(updatedProduct.name()).isEqualTo("Edited Product");
+        assertThat(updatedProduct.category()).isEqualTo(Category.TECH);
+        assertThat(updatedProduct.rating()).isEqualTo(3);
+    }
+
+    @Test
+    void testEditProductThatDoesNotExist() {
+        warehouse.editProduct(1, "Not existing product", Category.SPORT, 1);
+        var products = warehouse.getAllProducts();
+
+        assertThat(products).hasSize(0);
+    }
+
+    @Test
+    void testEditProductWithEmptyName() {
+        Product product = new Product(1, "Product", Category.SPORT, 5);
+        warehouse.addProduct(product);
+        warehouse.editProduct(1, "", Category.TECH, 5);
+
+        var optionalProduct = warehouse.getProductByID(1);
+        assertThat(optionalProduct).isPresent();
+
+        Product updatedProduct = optionalProduct.get();
+        assertThat(updatedProduct.name()).isEqualTo("Product");
+    }
+
+    // Get all products tests
+    @Test
+    void testGetAllProducts() {
+        warehouse.addProduct(new Product(1, "Ring", Category.JEWELRY, 5));
+        warehouse.addProduct(new Product(2, "Ball", Category.SPORT, 5));
+
+        var products = warehouse.getAllProducts();
+        assertThat(products).hasSize(2);
+    }
+
+    @Test
+    void testGetAllProductsFromEmptyWarehouse() {
+        var products = warehouse.getAllProducts();
+        assertThat(products).hasSize(0);
+    }
+
+    // Get product by id tests
+    @Test
+    void testGetProductById() {
+        var product = new Product(1, "Product 1", Category.TECH, 8);
+        warehouse.addProduct(product);
+
+        var optionalProduct = warehouse.getProductByID(1);
+        assertThat(optionalProduct).isPresent();
+        assertThat(optionalProduct.get()).isEqualTo(product);
+    }
+
+    // Get products by category and sort in alphabetic order
+    @Test
+    void TestGetProductsByCategoryAndSortedByAlphabeticOrder() {
+        warehouse.addProduct(new Product(1, "Product C", Category.TECH, 8));
+        warehouse.addProduct(new Product(2, "Product A", Category.JEWELRY, 7));
+        warehouse.addProduct(new Product(3, "Product B", Category.TECH, 9));
+
+        var categoryTechProducts = warehouse.getProductsByCategorySortedByName(Category.TECH);
+        assertThat(categoryTechProducts).hasSize(2);
+        assertThat(categoryTechProducts).extracting(Product::name).containsExactly("Product B", "Product C");
+    }
+
+    // Get products created after a date test
+    @Test
+    void getProductsCreatedAfterDate() {
+        var currentDate = LocalDateTime.now();
+        var pastDate = currentDate.minusDays(1);
+        var futureDate = currentDate.plusDays(1);
+
+        warehouse.addProduct(new Product(1, "Product 1", Category.TECH, 8, pastDate, pastDate));
+        warehouse.addProduct(new Product(2, "Product 2", Category.JEWELRY, 5, futureDate, futureDate));
+        warehouse.addProduct(new Product(3, "Product 3", Category.SPORT, 9, currentDate, currentDate));
+
+        var productsRecentlyCreated = warehouse.getProductsCreatedAfterDate(currentDate);
+        assertThat(productsRecentlyCreated).hasSize(1);
+        assertThat(productsRecentlyCreated.get(0).name()).isEqualTo("Product 2");
+    }
+
+    // Get products that have been modified after creation test
+    @Test
+    void getProductsModifiedAfterCreation() {
+        var currentDate = LocalDateTime.now();
+        var pastDate = currentDate.minusDays(1);
+        var futureDate = currentDate.plusDays(1);
+
+        warehouse.addProduct(new Product(1, "Product 1", Category.TECH, 8, pastDate, pastDate));
+        warehouse.addProduct(new Product(2, "Product 2", Category.JEWELRY, 7, futureDate, futureDate));
+        warehouse.addProduct(new Product(3, "Product 3", Category.SPORT, 9, currentDate, futureDate));
+
+        var productsModifiedAfterCreation = warehouse.getProductsModifiedAfterCreation();
+        assertThat(productsModifiedAfterCreation).hasSize(1);
+        assertThat(productsModifiedAfterCreation.get(0).name()).isEqualTo("Product 3");
     }
 }
